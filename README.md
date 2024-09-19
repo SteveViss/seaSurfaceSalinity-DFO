6076f2ce0d594d043dfabad334b3befefb4f17d5
SteveViss <steve.vissault@insileco.io>
2024-09-19 13:23:24 -0400

    Improve documentation

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 
 By Julien Laliberté et Steve Vissault
 
-[Access the main documentation](https://steveviss.github.io/seaSurfaceSalinity-DFO/)
+## Content
+
+- `interpolation/0-neighbors-approximation.qmd`: Describes using a neighbor-based method to fill missing values (NAs) in salinity data by applying spatial and temporal windows. Loads rasters and applies the approximation function defined in an external script (`scripts/spacetime_neighbors_approximation.R`).
+  
+- `interpolation/1-training-set.qmd`: Details the creation of a training set for Random Forest interpolation by sampling salinity data into bins (50 intervals, 5 samples per bin). 
+
+- `interpolation/2-random-forest.qmd`: Covers Random Forest model training with hyperparameter tuning using the ranger package.
+
+- `interpolation/3-validation.qmd`: Focuses on validating the model by computing RMSE using in situ data.
+
+- `interpolation/4-figures.qmd`: Generates figures for model results, setting parameters and directories for saving outputs.
+Loads reference data and prepares the visualization.
 
 ## Development
 
--- scripts/spacetime_neighbors_approximation.R
+++ scripts/spacetime_neighbors_approximation.R
@@ -1,12 +1,31 @@
-#' Set focal matrix
+#' Set Spatial Focal Window (Footprint)
 #'
-#' This function helps to declare a focal, matrix filled with 0 and 1: 0 meaning the neighbors cell is not include within the focal and 1 the opposite
-#' The matrix could also be filled with float values representative of the weight of cell.  
+#' This function creates a matrix representing a spatial window or focal footprint 
+#' to be used in spatial filtering or other neighborhood-based operations. The 
+#' footprint defines the neighborhood around each cell, with an option to exclude 
+#' the centroid.
 #'
-#' @param pixels Integer corresponding of the nrow and ncol of the focal matrix.
-#' @param exclude_centroid Boolean, does the centroid should be excluded from the focal?
-#' @returns A matrix representative of the neighbors cells included in the focal
+#' @param n_neighbors Integer. The number of neighboring cells in each direction 
+#' from the focal point (i.e., the radius of the neighborhood). The total size 
+#' of the focal window will be \code{(2 * n_neighbors + 1) x (2 * n_neighbors + 1)}. 
+#' Default is 3.
+#' @param exclude_centroid Logical. If \code{TRUE}, the central element of the 
+#' focal window (representing the focal cell itself) will be set to 0, excluding 
+#' it from calculations. If \code{FALSE}, the centroid will be included in the 
+#' window. Default is \code{TRUE}.
 #'
+#' @return A matrix of size \code{(2 * n_neighbors + 1) x (2 * n_neighbors + 1)} 
+#' representing the spatial footprint. The matrix consists of 1s, with the option 
+#' to exclude the central (focal) element by setting it to 0.
+#'
+#' @examples
+#' # Create a 3x3 focal window with the centroid excluded
+#' set_space_focal(n_neighbors = 1, exclude_centroid = TRUE)
+#'
+#' # Create a 5x5 focal window, including the centroid
+#' set_space_focal(n_neighbors = 2, exclude_centroid = FALSE)
+#'
+#' @export
 set_space_focal <- function(n_neighbors = 3, exclude_centroid = TRUE){
     # Set space window aka focal footprint
     focalSpace <- matrix(rep(1, (n_neighbors * 2 + 1)), nrow = (n_neighbors * 2 + 1), ncol = (n_neighbors * 2 + 1))
@@ -17,14 +36,42 @@ set_space_focal <- function(n_neighbors = 3, exclude_centroid = TRUE){
     return(focalSpace)
 }
 
-#' For NA pixels, set values based on neigbors in space and time
+#' Approximate variable by Spatio-Temporal Neighbors
 #'
-#' This function average in space and time
+#' This function approximates spatio-temporal neighbors by selecting a subset of 
+#' layers from a time series raster stack, applying a focal operation over a specified 
+#' spatial window for each time step, and then aggregating the results over the 
+#' specified time window.
 #'
-#' @param pixels Integer corresponding of the nrow and ncol of the focal matrix.
-#' @param exclude_centroid Boolean, does the centroid should be excluded from the focal?
-#' @returns 
+#' @param stack A \code{SpatRaster} object. The input raster stack with a time dimension.
+#' @param target_time A \code{character} or \code{Date}. The target time around which 
+#' to extract layers, specified as "YYYY-MM-DD". Default is "2015-04-04".
+#' @param time_window A numeric vector of length 2. The time window (in days) around 
+#' the \code{target_time} within which layers are selected. The first element defines 
+#' the lower bound (e.g., \code{-1} means one day before), and the second element 
+#' defines the upper bound (e.g., \code{1} means one day after). Default is \code{c(-1, 1)}.
+#' @param space_focal A matrix. The spatial window (focal footprint) applied to each 
+#' layer in the time window. By default, it uses \code{set_space_focal()} to create a 
+#' spatial focal window.
+#' @param fun A function. The aggregation function applied over the spatial and temporal 
+#' dimensions. The default is \code{mean}.
+#' @param verbose Logical. If \code{TRUE}, the function prints progress and messages. 
+#' Default is \code{FALSE}.
+#'
+#' @return A \code{SpatRaster} object representing the aggregated result of applying the 
+#' focal operation over the selected time window and averaging the results over both 
+#' space and time.
+#'
+#' @examples
+#' # Example usage with a SpatRaster stack
+#' # Assuming 'stack' is a SpatRaster object with time metadata
+#' approximate_st_neighbors(stack, target_time = "2015-04-04", time_window = c(-2, 2), 
+#'                          space_focal = set_space_focal(2), fun = mean, verbose = TRUE)
+#'
+#' @seealso \code{\link[terra]{focal}}, \code{\link[terra]{time}}, \code{\link[terra]{app}}
 #' 
+#' @importFrom terra time nlyr subset rast app focal
+#' @export
 approximate_st_neighbors <- function(stack, target_time = "2015-04-04", time_window = c(-1, 1), space_focal = set_space_focal(), fun = mean, verbose = FALSE) {
 
     stopifnot(any(!is.na(terra::time(stack))))
@@ -49,8 +96,7 @@ approximate_st_neighbors <- function(stack, target_time = "2015-04-04", time_win
     return(aggregated_layer)
 }
 
-# Apply approximation by neighbors
-# Wrapper function
+# This function perform the neighbors approximation along the raster stack for each timestamp
 apply_st_neighbors_on_stack <- function(stack, timestamps, target_folder, n_cores, n_neighbors, temp_window, agg_fun){
 
     outputs_folder <- file.path(target_folder, glue::glue("TB_{n_neighbors}pixels_{temp_window}days"))
@@ -68,53 +114,4 @@ apply_st_neighbors_on_stack <- function(stack, timestamps, target_folder, n_core
     future::plan("sequential") 
 }
 
-OC_rs_path <- list.files("data/raster/OC", pattern = "*.tif", full.names = TRUE) 
-TB_rs_path <- list.files("data/raster/TB", pattern = "*.tif", full.names = TRUE)
-
-OC_files <- data.frame(
-    OC_path = OC_rs_path,
-    timestamp = as.Date(stringr::str_extract(OC_rs_path, "[0-9]{4}-[0-9]{2}-[0-9]{2}"))
-)
-
-TB_files <- data.frame(
-    TB_path = TB_rs_path,
-    timestamp = as.Date(stringr::str_extract(TB_rs_path, "[0-9]{4}-[0-9]{2}-[0-9]{2}"))
-)
-
-# Apply the approximation function on TB stack
-TB_rasters <- terra::rast(TB_files$TB_path)
-terra::time(TB_rasters) <- TB_files$timestamp
-
-# Run for 5 pixels in space and 5 days in time
-apply_st_neighbors_on_stack(
-    stack = TB_rasters,
-    timestamps = TB_files$timestamp, 
-    target_folder = "/home/steve/Documents/outputs_DFO", 
-    n_cores = 8, 
-    n_neighbors = 5, 
-    temp_window = 5, 
-    agg_fun = mean
-)
-
-# Run for 3 pixels in space and 3 days in time
-apply_st_neighbors_on_stack(
-    stack = TB_rasters,
-    timestamps = TB_files$timestamp, 
-    target_folder = "/home/steve/Documents/outputs_DFO", 
-    n_cores = 8, 
-    n_neighbors = 3, 
-    temp_window = 3, 
-    agg_fun = mean
-)
-
-# Run for 1 pixels in space and 1 days in time
-apply_st_neighbors_on_stack(
-    stack = TB_rasters,
-    timestamps = TB_files$timestamp, 
-    target_folder = "/home/steve/Documents/outputs_DFO", 
-    n_cores = 8, 
-    n_neighbors = 1, 
-    temp_window = 1, 
-    agg_fun = mean
-)
 
